224be4aefd4aeb2a8a2a06253daae5221fa01276
James DeVries <jamesrdevries@gmail.com>
2025-02-12 06:59:11 -0600

    Implement paging and cursor navigation

--- textedit.go
+++ textedit.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -10,9 +11,12 @@ import (
 
 type model struct {
 	content   []string
-	cursor_x  int
-	virtual_x int
-	cursor_y  int
+	cursorX   int
+	virtualX  int
+	cursorY   int
+	height    int
+	width     int
+	pageStart int
 }
 
 func (m model) Init() tea.Cmd {
@@ -21,35 +25,67 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.height = msg.Height
+		m.width = msg.Width
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
+
+		switch key := msg.String(); key {
+		// application control
+		case "ctrl+c", "ctrl+q":
 			return m, tea.Quit
+
+		// navigation
 		case "up":
-			if m.cursor_y > 0 {
-				m.cursor_y--
+			if m.cursorY > 0 {
+				m.cursorY--
 			}
+		case "pgup":
+			m.cursorY = max(0, m.cursorY-m.height/2)
 
 		case "down":
-			if m.cursor_y < len(m.content)-1 {
-				m.cursor_y++
+			if m.cursorY < len(m.content)-1 {
+				m.cursorY++
 			}
+		case "pgdown":
+			m.cursorY = min(len(m.content)-1, m.cursorY+m.height/2)
+
+		case "home":
+			m.cursorX = 0
+			m.virtualX = m.cursorX
+
+		case "end":
+			m.cursorX = len(m.content[m.cursorY]) - 1
+			m.virtualX = m.cursorX
 
 		case "left":
-			if m.cursor_x > 0 {
-				m.cursor_x--
-				m.virtual_x = m.cursor_x
+			if m.cursorX > 0 {
+				m.cursorX--
+				m.virtualX = m.cursorX
 			}
 		case "right":
-			if m.cursor_x < len(m.content[m.cursor_y])-1 {
-				m.cursor_x++
-				m.virtual_x = m.cursor_x
+			if m.cursorX < len(m.content[m.cursorY])-1 {
+				m.cursorX++
+				m.virtualX = m.cursorX
 			}
+
+		// text editing
+		case "enter":
+			// TODO: break line
+			m.content = slices.Insert(m.content, m.cursorY, "")
+		default:
+			m.content = slices.Insert(m.content, m.cursorY, key)
 		}
-		max_x := max(len(m.content[m.cursor_y])-1, 0)
-		m.cursor_x = min(max_x, max(m.cursor_x, m.virtual_x))
+		max_x := max(len(m.content[m.cursorY])-1, 0)
+		m.cursorX = min(max_x, max(m.cursorX, m.virtualX))
+
+		// set up the pageStart
+		if m.cursorY < m.pageStart {
+			m.pageStart = m.cursorY
+		} else if m.cursorY >= m.pageStart+m.height-2 {
+			m.pageStart = max(0, m.cursorY-(m.height-2))
+		}
 
 	}
 	return m, nil
@@ -57,12 +93,17 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m model) View() string {
 	var sb strings.Builder
-	for y, line := range m.content {
-		if y == m.cursor_y && len(line) == 0 {
+
+	for y := m.pageStart; y < m.pageStart+m.height-1; y++ {
+		line := " -"
+		if y < len(m.content) {
+			line = m.content[y]
+		}
+		if y == m.cursorY && len(line) == 0 {
 			sb.WriteString(">\n")
 		} else {
 			for x, c := range line {
-				if x == m.cursor_x && y == m.cursor_y {
+				if x == m.cursorX && y == m.cursorY {
 					sb.WriteRune('>')
 				} else {
 					sb.WriteRune(c)
@@ -76,15 +117,15 @@ func (m model) View() string {
 
 func initModelWithText(s string) model {
 	return model{
-		content:   strings.Split(s, "\n"),
-		cursor_y:  0,
-		virtual_x: 0,
-		cursor_x:  0,
+		content:  strings.Split(s, "\n"),
+		cursorY:  0,
+		virtualX: 0,
+		cursorX:  0,
 	}
 }
 
 func main() {
-	p := tea.NewProgram(initModelWithText("Hello!\n\nPress q to quit."))
+	p := tea.NewProgram(initModelWithText("Hello!\n\nPress ctrl+q to quit."))
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
